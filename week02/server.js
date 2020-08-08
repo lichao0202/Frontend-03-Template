7f678e65163e35b27e0f25f176c93723d0b91df6
lichao.franklee <lichao.franklee@gmail.com>
2020-08-08 22:39:11 +0800

    feature: homework of week02

---
+++ week02/server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+http.createServer((request, response) => {
+  let body = []
+  request
+    .on('error', (err) => {
+      console.log(err)
+    })
+    .on('data', (chunk) => {
+      body.push(Buffer.from(chunk))
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString()
+      console.log('body:', body)
+      response.writeHead(200, {'Content-Type': 'text/html'})
+      response.end(' Hello world\n')
+    })
+}).listen(8088)
